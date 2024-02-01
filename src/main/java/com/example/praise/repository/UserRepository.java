d33187f43af8c1151c5c91e3e283fd5892f0ce33
wkdyujin <yujin1203713@gmail.com>
2024-02-01 16:39:22 +0900

    feat change password

--- src/main/java/com/example/praise/repository/UserRepository.java
+++ src/main/java/com/example/praise/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.praise.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,5 +10,5 @@ import com.example.praise.model.entity.User;
 public interface UserRepository extends JpaRepository<User, Integer>{
 
 	User findByUsername(String username); // SELECT * FROM User WHERE username  = ?
-
+	Optional<User> findByIdAndPassword(int id, String password);
 }
